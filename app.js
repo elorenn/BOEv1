8cbb696475d1130a45a4a58cd759fb4c7b54fd40
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-12-15 11:02:34 -0500

    fixed repetitions from merging

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
-const mongoose = require("mongoose");
 const https = require("https");
 const File = require("./model/fileSchema");
 const multer = require("multer");
@@ -12,10 +11,6 @@ const schools = require("./business.json");
 const app = express();
 
 // Set up code
-app.set("view engine", "ejs");
-app.set("views", "views");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
@@ -44,10 +39,8 @@ function searchbarfunction() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("searchInput");
-  input = document.getElementById("searchInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("gallery1");
-  li = ul.getElementsByTagName("li");
   li = ul.getElementsByTagName("li");
 
   // Loop through all list items, and hide those who don't match the search query
@@ -65,28 +58,21 @@ function searchbarfunction() {
 // Connect to Mongo DB
 const mongoURL =
   "mongodb+srv://boepartners:missyangus123@cluster0.dm8gvgf.mongodb.net/BOE";
-const mongoURL =
-  "mongodb+srv://boepartners:missyangus123@cluster0.dm8gvgf.mongodb.net/BOE";
 
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const BOESchema = new mongoose.Schema({
   Organization: String,
-  Organization: String,
   First_Name: {
     type: String,
-    required: [false, "Please enter first name"],
     required: [false, "Please enter first name"],
   },
   Last_Name: {
     type: String,
     required: [false, "Please enter last name"],
-    required: [false, "Please enter last name"],
   },
   Email: {
     type: String,
-    required: [false, "Please enter email"],
     required: [false, "Please enter email"],
   },
   AppliedForTrade: String,
@@ -98,10 +84,8 @@ const BOESchema = new mongoose.Schema({
   Zipcode: {
     type: Number,
     required: [false, "Please enter numeric value"],
-    required: [false, "Please enter numeric value"],
   },
   Additional_Comments: String,
-  Date: String,
   Date: String,
 });
 
@@ -120,12 +104,9 @@ const multerFilter = (req, file, cb) => {
   if (file.mimetype.split("/")[1] === "jpeg") {
     cb(null, true);
   } else if (file.mimetype.split("/")[1] === "jpg") {
-  } else if (file.mimetype.split("/")[1] === "jpg") {
     cb(null, true);
   } else if (file.mimetype.split("/")[1] === "png") {
-  } else if (file.mimetype.split("/")[1] === "png") {
     cb(null, true);
-  } else if (file.mimetype.split("/")[1] === "img") {
   } else if (file.mimetype.split("/")[1] === "img") {
     cb(null, true);
   } else {
@@ -145,12 +126,6 @@ app.post("/", upload.single("resume"), async (req, res) => {
     month: "numeric",
     day: "numeric",
   });
-app.post("/", upload.single("resume"), async (req, res) => {
-  const postedDate = new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
   const Application = mongoose.model("Application", BOESchema);
   const application = new Application({
     Organization: req.body.organization,
@@ -159,7 +134,6 @@ app.post("/", upload.single("resume"), async (req, res) => {
     Email: req.body.email,
     AppliedForTrade: req.body.appliedForTrade,
     Additional_Comments: req.body.additionalcomments,
-    Date: postedDate,
     Date: postedDate,
   });
   application.save();
@@ -181,20 +155,6 @@ app.post("/index.html2", function (req, res) {
   external_applicant.save();
   res.redirect(req.body.orgApplicationURL);
 });
-app.post("/index.html2", function (req, res) {
-  const postedDate = new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
-  const External_Applicant = mongoose.model("External_Applicant", BOESchema);
-  const external_applicant = new External_Applicant({
-    Organization: req.body.organization,
-    Date: postedDate,
-  });
-  external_applicant.save();
-  res.redirect(req.body.orgApplicationURL);
-});
 
 // Subcriber Page
 app.post("/usersignup", function (req, res) {
@@ -211,36 +171,7 @@ app.post("/usersignup", function (req, res) {
     month: "numeric",
     day: "numeric",
   });
-// Subcriber Page
-app.post("/usersignup", function (req, res) {
-  const userFirstName = req.body.fname;
-  const userLastName = req.body.lname;
-  const userEmail = req.body.email;
-  const userCity = req.body.city;
-  const zipCode = req.body.zipcode;
-  const trade1 = req.body.trade1;
-  const trade2 = req.body.trade2;
-  const trade3 = req.body.trade3;
-  const postedDate = new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
 
-  // store in BOE database
-  const Subscriber = mongoose.model("Subscriber", BOESchema);
-  const subscriber = new Subscriber({
-    First_Name: userFirstName,
-    Last_Name: userLastName,
-    Email: userEmail,
-    City: userCity,
-    Zipcode: zipCode,
-    SubscriberTradeOfInterest1: trade1,
-    SubscriberTradeOfInterest2: trade2,
-    SubscriberTradeOfInterest3: trade3,
-    Date: postedDate,
-  });
-  subscriber.save();
   // store in BOE database
   const Subscriber = mongoose.model("Subscriber", BOESchema);
   const subscriber = new Subscriber({
@@ -292,52 +223,13 @@ app.post("/usersignup", function (req, res) {
   });
   request.write(jsonData);
   request.end();
-  // send to Mailchimp
-  const data = {
-    members: [
-      {
-        email_address: userEmail,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: userFirstName,
-          LNAME: userLastName,
-          CITY: userCity,
-          ZIPCODE: zipCode,
-          TRADE1: trade1,
-          TRADE2: trade2,
-          TRADE3: trade3,
-        },
-      },
-    ],
-  };
-  const jsonData = JSON.stringify(data);
-  const url = "https://us17.api.mailchimp.com/3.0/lists/a61fc42e0a";
-  const options = {
-    method: "POST",
-    auth: "HenryC:a1c299a97ae75e0e005592e3fa618060-us17",
-  };
-  const request = https.request(url, options, function (response) {
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.ejs");
-    } else {
-      res.sendFile(__dirname + "/failure.ejs");
-    }
-    response.on("data", function (data) {
-      console.log(JSON.parse(data));
-    });
-  });
-  request.write(jsonData);
-  request.end();
 });
-app.post("/failure", function (req, res) {
 app.post("/failure", function (req, res) {
   res.redirect("/subscribe.ejs");
 });
 
 app.post("/success", function (req, res) {
-app.post("/success", function (req, res) {
   res.redirect("/");
 });
 
-module.exports = app;
 module.exports = app;

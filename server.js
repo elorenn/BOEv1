6cc9355b625506db4e4134577370a5984ffeae85
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-12-15 23:41:09 -0500

    login takes you to user profile page

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post(
   "/login",
   checkNotAuthenticated,
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/profile",
     failureRedirect: "/login",
     failureFlash: true,
   })

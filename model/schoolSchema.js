23df37e046671f5983c1a0c9ab4f209d7b39457e
Eddy DuQuesne <eddy.duquesne@getbuilt.com>
2022-12-22 18:00:33 -0600

    add schools to db

---
+++ model/schoolSchema.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  number: {
+    type: String,
+  },
+  email: {
+    type: String
+  },
+  contactUrl: {
+    type: String
+  },
+});
+
+const LocationSchema = new mongoose.Schema({
+  state: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true,
+  }
+});
+
+
+const SchoolSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  logoUrl: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  siteUrl: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  applyUrl: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  type: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  location: LocationSchema,
+  pointOfContact: ContactSchema,
+  programs: {
+    type: Array
+  }
+});
+
+const SchoolModel = mongoose.model("School", SchoolSchema);
+
+exports.School = SchoolModel;
+exports.SchoolSchema = SchoolSchema;

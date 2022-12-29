65daedec3acb830befe84e2babb60f21fe985db3
Lorena Sarasua <lorena.sarasua@gmail.com>
2022-12-29 00:31:22 -0500

    remove unused Joi code. Remove unused favorites array from schema since favorites have their own schema/are a separate collection in the DB

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,4 +1,3 @@
-// const Joi = require("joi");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
@@ -22,20 +21,9 @@ const UserSchema = new mongoose.Schema({
     maxlength: null,
   },
   date: String,
-  favorites: [],
 });
 
 const UserModel = mongoose.model("User", UserSchema);
 
-// function validateUser(user) {
-//   const schema = {
-//     name: Joi.string().min(1).max(20).required(),
-//     email: Joi.string().min(2).max(20).required().email(),
-//     password: Joi.string().min(1).max(7).required(),
-//   };
-//   return Joi.validate(user, schema);
-// }
-
 exports.UserModel = UserModel;
 exports.UserSchema = UserSchema;
-// exports.validate = validateUser;

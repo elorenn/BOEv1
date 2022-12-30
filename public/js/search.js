e941a6a49a25a59cd51ffb1194cfa9ab41992e42
Lorena Sarasua <lorena.sarasua@gmail.com>
2022-12-30 04:14:23 -0500

    check the display of each school. if none of the schools (li elements) are displayed, then show the empty error message.

--- public/js/search.js
+++ public/js/search.js
@@ -1,11 +1,12 @@
 // Home page search bar
 function searchBar() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i, txtValue, empty, listArr, check;
   input = document.getElementById("searchInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("lo-basic-list");
   li = ul.getElementsByTagName("li");
+  empty = document.getElementById("lo-search-empty");
+  listArr = [...li];
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
@@ -17,4 +18,10 @@ function searchBar() {
       li[i].style.display = "none";
     }
   }
+
+  check = listArr.every((e) => {
+    return e.style.display === "none";
+  });
+  // if none of the schools (li elements) are displayed, then show the empty error message.
+  check ? (empty.style.display = "unset") : (empty.style.display = "none");
 }

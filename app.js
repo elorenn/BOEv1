5afca4b52d6d2ceeb39bc253bbbd1c96f362d5d2
Lorena Sarasua <lorena.sarasua@gmail.com>
2023-01-01 05:19:38 -0500

    changed routing for app submitted page

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
-const https = require("https");
 const File = require("./model/fileSchema");
 const multer = require("multer");
-const http = require("http");
-const session = require("express-session");
-// const { UserLike } = require("./model/userLikeSchema");
-
-// const UserSchema = require("./model/userSchema");
 
 const app = express();
 
@@ -19,18 +13,7 @@ app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/applicationSubmitted.html", (req, res) => {
-  res.sendFile(__dirname + "/views/applicationSubmitted.html");
-});
-
-// Connect to Mongo DB
-// const mongoURL =
-//   "mongodb+srv://boepartners:missyangus123@cluster0.dm8gvgf.mongodb.net/BOE";
-
-// mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// --------------------------------------------------------------------- //
-// Subscribe Page Schema
+// -------------------------------  Subscribe Page Schema -------------------------------------- //
 
 const BOESchema = new mongoose.Schema({
   Organization: String,
@@ -73,23 +56,8 @@ const multerStorage = multer.diskStorage({
   },
 });
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.split("/")[1] === "pdf") {
-    cb(null, true);
-  } else if (file.mimetype.split("/")[1] === "jpg") {
-    cb(null, true);
-  } else if (file.mimetype.split("/")[1] === "png") {
-    cb(null, true);
-  } else if (file.mimetype.split("/")[1] === "img") {
-    cb(null, true);
-  } else {
-    cb(new Error("Not an image!!"), false);
-  }
-};
-
 const upload = multer({
   storage: multerStorage,
-  fileFilter: multerFilter,
 });
 
 // Get application data
@@ -110,7 +78,7 @@ app.post("/", upload.single("resume"), async (req, res) => {
     Date: postedDate,
   });
   application.save();
-  res.redirect("/applicationSubmitted.html");
+  res.redirect("/applicationSubmitted");
 });
 
 // Redirect to external application page, store user data
@@ -158,43 +126,6 @@ app.post("/usersignup", function (req, res) {
   } catch {
     res.redirect("/failure");
   }
-
-  // // send to Mailchimp
-  // const data = {
-  //   members: [
-  //     {
-  //       email_address: userEmail,
-  //       status: "subscribed",
-  //       merge_fields: {
-  //         FNAME: userFirstName,
-  //         LNAME: userLastName,
-  //         CITY: userCity,
-  //         ZIPCODE: zipCode,
-  //         TRADE1: trade1,
-  //         TRADE2: trade2,
-  //         TRADE3: trade3,
-  //       },
-  //     },
-  //   ],
-  // };
-  // const jsonData = JSON.stringify(data);
-  // const url = "https://us17.api.mailchimp.com/3.0/lists/a61fc42e0a";
-  // const options = {
-  //   method: "POST",
-  //   auth: "HenryC:a1c299a97ae75e0e005592e3fa618060-us17", // this key should not be public or in GitHub
-  // };
-  // const request = https.request(url, options, function (response) {
-  //   if (response.statusCode === 200) {
-  //     res.sendFile(__dirname + "/success.ejs");
-  //   } else {
-  //     res.sendFile(__dirname + "/failure.ejs");
-  //   }
-  //   response.on("data", function (data) {
-  //     console.log(JSON.parse(data));
-  //   });
-  // });
-  // request.write(jsonData);
-  // request.end();
 });
 
 app.post("/failure", function (req, res) {
@@ -204,38 +135,5 @@ app.post("/failure", function (req, res) {
 app.post("/success", function (req, res) {
   res.redirect("/"); // is this doing anything?
 });
-
-// -------------------------------- USER LIKES FAVORITES ------------------------------------- //
-
-// app.post("/user-likes", async function (req, res) {
-//   const user = req.app.get("user");
-//   const schoolId = req.body.school_id;
-
-//   if (!user || !user.id || !schoolId) {
-//     if (!user) {
-//       // user not logged in
-//       res.redirect("/login");
-//     }
-//     return null; // @TO-DO: handle errors
-//   }
-
-//   const query = {
-//     school_id: schoolId,
-//     user_id: user.id,
-//   };
-
-//   const userLike = await UserLike.findOne(query);
-//   const isLiked = userLike ? !userLike.is_liked : true;
-
-//   const userLikeData = {
-//     user_id: user.id,
-//     school_id: schoolId,
-//     is_liked: isLiked,
-//   };
-
-//   const options = { upsert: true };
-//   await UserLike.findOneAndUpdate(query, userLikeData, options);
-//   res.redirect("back");
-// });
 
 module.exports = app;

15d3f28328afd158722e1bfe709a43b34a63aacc
Lorena Sarasua <lorena.sarasua@gmail.com>
2023-01-02 07:40:51 -0500

    validate username so it only contains letters, no numbers or symbols

--- model/userSchema.js
+++ model/userSchema.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z]+$/.test(v);
+      },
+      message: (props) =>
+        `should only contain letters. No special characters or numbers.`,
+    },
     required: true,
     minlength: 1,
     maxlength: [20, "Cannot be longer than 20 characters."],

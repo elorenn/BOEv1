d7b62840456ef5881d78e78a094d9d702abe5c30
Lorena Sarasua <lorena.sarasua@gmail.com>
2023-01-01 22:48:19 -0500

    basic list remains highlighted if selected/visible in depth list

--- public/js/listings.js
+++ public/js/listings.js
@@ -1,12 +1,27 @@
-function toggleClass(element) {
-  if (element.classList.contains("show")) {
-    element.classList.remove("show");
+function toggleClass(depthSchool) {
+  if (depthSchool.classList.contains("show")) {
+    depthSchool.classList.remove("show");
   } else {
-    element.classList.add("show");
+    depthSchool.classList.add("show");
+  }
+}
+
+function toggleAria(basicSchool) {
+  let basicLink = basicSchool.children[0];
+  let links = basicSchools.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    console.log(links[i]);
+    if (links[i] === basicLink) {
+      links[i].ariaCurrent = "location";
+    } else {
+      links[i].ariaCurrent = "false";
+    }
   }
 }
 
 const basicSchools = document.getElementById("lo-basic-list");
+const basicSchools2 = document.querySelectorAll("#lo-basic-list");
+
 const depthSchoolsPage = document.getElementById("lo-in-depth-list");
 const depthSchools = depthSchoolsPage.querySelectorAll("li");
 
@@ -14,11 +29,13 @@ let basicId, depthListItem;
 
 basicSchools.addEventListener("click", function (event) {
   if (event.target.closest(".lo-basic-list-item")) {
-    basicId = event.target.closest(".lo-basic-list-item").dataset.id;
+    basicSchool = event.target.closest(".lo-basic-list-item");
+    basicId = basicSchool.dataset.id;
     depthSchools.forEach((school) => {
       school.classList.remove("show");
       if (basicId === school.dataset.id) {
         toggleClass(school);
+        toggleAria(basicSchool);
       }
     });
   }

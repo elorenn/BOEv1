d7540c92b3dfd9a0931e8f31b8ce00e5abcef28e
Lorena Sarasua <lorena.sarasua@gmail.com>
2023-01-03 01:31:43 -0500

    tentatively moved subscribe logic from app to server in order to show error validations. Wired validations.

--- app.js
+++ app.js
@@ -13,127 +13,148 @@ app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// -------------------------------  Subscribe Page Schema -------------------------------------- //
+// // -------------------------------  Subscribe Page Schema -------------------------------------- //
 
-const BOESchema = new mongoose.Schema({
-  Organization: String,
-  First_Name: {
-    type: String,
-    required: [false, "Please enter first name"],
-  },
-  Last_Name: {
-    type: String,
-    required: [false, "Please enter last name"],
-  },
-  Email: {
-    type: String,
-    required: [false, "Please enter email"],
-  },
-  AppliedForTrade: String,
-  Resume: String,
-  SubscriberTradeOfInterest1: String,
-  SubscriberTradeOfInterest2: String,
-  SubscriberTradeOfInterest3: String,
-  City: String,
-  Zipcode: {
-    type: Number,
-    required: [false, "Please enter numeric value"],
-  },
-  Additional_Comments: String,
-  Date: String,
-});
+// const BOESchema = new mongoose.Schema({
+//   Organization: String,
+//   First_Name: {
+//     type: String,
+//     required: [true, "Please enter first name"],
+//     validate: {
+//       validator: function (v) {
+//         return /^[a-zA-Z]+$/.test(v);
+//       },
+//       message: (props) =>
+//         `should only contain letters. No special characters or numbers.`,
+//     },
+//   },
+//   Last_Name: {
+//     type: String,
+//     required: [false, "Please enter last name"],
+//     validate: {
+//       validator: function (v) {
+//         return /^[a-zA-Z]+$/.test(v);
+//       },
+//       message: (props) =>
+//         `should only contain letters. No special characters or numbers.`,
+//     },
+//   },
+//   Email: {
+//     type: String,
+//     required: [false, "Please enter email"],
+//   },
+//   AppliedForTrade: String,
+//   Resume: String,
+//   SubscriberTradeOfInterest1: String,
+//   SubscriberTradeOfInterest2: String,
+//   SubscriberTradeOfInterest3: String,
+//   City: String,
+//   Zipcode: {
+//     type: Number,
+//     required: [true, "Please enter numeric value"],
+//     validate: {
+//       validator: function (v) {
+//         return /^[1-9]+$/.test(v);
+//       },
+//       message: (props) => `should only contain numbers.`,
+//     },
+//   },
+//   Additional_Comments: String,
+//   Date: String,
+// });
 
-// --------------------------------------------------------------------- //
+// // --------------------------------------------------------------------- //
 
-// Multer file storage
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public");
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `files/${req.body.organization + "_" + req.body.email}.${ext}`);
-  },
-});
+// // Multer file storage
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split("/")[1];
+//     cb(null, `files/${req.body.organization + "_" + req.body.email}.${ext}`);
+//   },
+// });
 
-const upload = multer({
-  storage: multerStorage,
-});
+// const upload = multer({
+//   storage: multerStorage,
+// });
 
-// Get application data
-app.post("/", upload.single("resume"), async (req, res) => {
-  const postedDate = new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
-  const Application = mongoose.model("Application", BOESchema);
-  const application = new Application({
-    Organization: req.body.organization,
-    First_Name: req.body.firstname,
-    Last_Name: req.body.lastname,
-    Email: req.body.email,
-    AppliedForTrade: req.body.appliedForTrade,
-    Additional_Comments: req.body.additionalcomments,
-    Date: postedDate,
-  });
-  application.save();
-  res.redirect("/applicationSubmitted");
-});
+// // Get application data
+// app.post("/", upload.single("resume"), async (req, res) => {
+//   const postedDate = new Date().toLocaleDateString("en-us", {
+//     year: "numeric",
+//     month: "numeric",
+//     day: "numeric",
+//   });
+//   const Application = mongoose.model("Application", BOESchema);
+//   const application = new Application({
+//     Organization: req.body.organization,
+//     First_Name: req.body.firstname,
+//     Last_Name: req.body.lastname,
+//     Email: req.body.email,
+//     AppliedForTrade: req.body.appliedForTrade,
+//     Additional_Comments: req.body.additionalcomments,
+//     Date: postedDate,
+//   });
+//   application.save();
+//   res.redirect("/applicationSubmitted");
+// });
 
-// Redirect to external application page, store user data
-app.post("/index.html2", function (req, res) {
-  const postedDate = new Date().toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "numeric",
-    // day: "numeric",
-  });
-  const External_Applicant = mongoose.model("External_Applicant", BOESchema);
-  const external_applicant = new External_Applicant({
-    Organization: req.body.organization,
-    Date: postedDate,
-  });
-  external_applicant.save();
-  res.redirect(req.body.orgApplicationURL);
-});
+// // Redirect to external application page, store user data
+// app.post("/index.html2", function (req, res) {
+//   const postedDate = new Date().toLocaleDateString("en-us", {
+//     year: "numeric",
+//     month: "numeric",
+//     day: "numeric",
+//   });
+//   const External_Applicant = mongoose.model("External_Applicant", BOESchema);
+//   const external_applicant = new External_Applicant({
+//     Organization: req.body.organization,
+//     Date: postedDate,
+//   });
+//   external_applicant.save();
+//   res.redirect(req.body.orgApplicationURL);
+// });
 
-// --------------------------------------------------------------------- //
-// Subcriber Page - Save to Database
+// // --------------------------------------------------------------------- //
+// // Subcriber Page - Save to Database
 
-app.post("/usersignup", function (req, res) {
-  try {
-    const postedDate = new Date().toLocaleDateString("en-us", {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    });
+// app.post("/usersignup", function (req, res) {
+//   try {
+//     const postedDate = new Date().toLocaleDateString("en-us", {
+//       year: "numeric",
+//       month: "numeric",
+//       day: "numeric",
+//     });
 
-    // store in BOE database
-    const Subscriber = mongoose.model("Subscriber", BOESchema);
-    const subscriber = new Subscriber({
-      First_Name: req.body.fname,
-      Last_Name: req.body.lname,
-      Email: req.body.email,
-      City: req.body.city,
-      Zipcode: req.body.zipcode,
-      SubscriberTradeOfInterest1: req.body.trade1,
-      SubscriberTradeOfInterest2: req.body.trade2,
-      SubscriberTradeOfInterest3: req.body.trade3,
-      Date: postedDate,
-    });
-    subscriber.save();
-    res.redirect("/success");
-  } catch {
-    res.redirect("/failure");
-  }
-});
+//     // store in BOE database
+//     const Subscriber = mongoose.model("Subscriber", BOESchema);
+//     const subscriber = new Subscriber({
+//       First_Name: req.body.fname,
+//       Last_Name: req.body.lname,
+//       Email: req.body.email,
+//       City: req.body.city,
+//       Zipcode: req.body.zipcode,
+//       SubscriberTradeOfInterest1: req.body.trade1,
+//       SubscriberTradeOfInterest2: req.body.trade2,
+//       SubscriberTradeOfInterest3: req.body.trade3,
+//       Date: postedDate,
+//     });
 
-app.post("/failure", function (req, res) {
-  res.redirect("/subscribe.ejs"); // is this doing anything?
-});
-
-app.post("/success", function (req, res) {
-  res.redirect("/"); // is this doing anything?
-});
+//     const err = subscriber.validateSync();
+//     if (err) {
+//       console.log(err.message);
+//       req.flash("subError", err.message);
+//       return res.redirect("/subscribe");
+//     } else {
+//       // validation passed
+//       subscriber.save();
+//       res.redirect("/success");
+//     }
+//   } catch {
+//     res.redirect("/failure");
+//   }
+// });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -72,11 +72,158 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
 
-// -------------------------------- ROUTES ------------------------------------- //
+// --------------------------- TEST
 
-// app.param("name", (req, res, next, name) => {
-//   params: req.params["name"],
-// });
+const multer = require("multer");
+
+// -------------------------------  Subscribe Page Schema -------------------------------------- //
+
+const BOESchema = new mongoose.Schema({
+  Organization: String,
+  First_Name: {
+    type: String,
+    required: [true, "Please enter first name"],
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z]+$/.test(v);
+      },
+      message: (props) =>
+        `should only contain letters. No special characters or numbers.`,
+    },
+  },
+  Last_Name: {
+    type: String,
+    required: [
+      true,
+      "Please enter last name. Should only contain letters. No special characters or numbers.",
+    ],
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z]+$/.test(v);
+      },
+      message: (props) =>
+        `should only contain letters. No special characters or numbers.`,
+    },
+  },
+  Email: {
+    type: String,
+    required: [true, "Please enter email"],
+  },
+  AppliedForTrade: String,
+  Resume: String,
+  SubscriberTradeOfInterest1: String,
+  SubscriberTradeOfInterest2: String,
+  SubscriberTradeOfInterest3: String,
+  City: String,
+  Zipcode: {
+    type: Number,
+    required: [true, "Please enter numeric value"],
+    validate: {
+      validator: function (v) {
+        return /^[1-9]+$/.test(v);
+      },
+      message: (props) => `should only contain numbers.`,
+    },
+  },
+  Additional_Comments: String,
+  Date: String,
+});
+
+// --------------------------------------------------------------------- //
+
+// Multer file storage
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `files/${req.body.organization + "_" + req.body.email}.${ext}`);
+  },
+});
+
+const upload = multer({
+  storage: multerStorage,
+});
+
+// Get application data
+app.post("/", upload.single("resume"), async (req, res) => {
+  const postedDate = new Date().toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  const Application = mongoose.model("Application", BOESchema);
+  const application = new Application({
+    Organization: req.body.organization,
+    First_Name: req.body.firstname,
+    Last_Name: req.body.lastname,
+    Email: req.body.email,
+    AppliedForTrade: req.body.appliedForTrade,
+    Additional_Comments: req.body.additionalcomments,
+    Date: postedDate,
+  });
+  application.save();
+  res.redirect("/applicationSubmitted");
+});
+
+// Redirect to external application page, store user data
+app.post("/index.html2", function (req, res) {
+  const postedDate = new Date().toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  const External_Applicant = mongoose.model("External_Applicant", BOESchema);
+  const external_applicant = new External_Applicant({
+    Organization: req.body.organization,
+    Date: postedDate,
+  });
+  external_applicant.save();
+  res.redirect(req.body.orgApplicationURL);
+});
+
+// --------------------------------------------------------------------- //
+// Subcriber Page - Save to Database
+
+app.post("/usersignup", function (req, res) {
+  try {
+    const postedDate = new Date().toLocaleDateString("en-us", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+
+    // store in BOE database
+    const Subscriber = mongoose.model("Subscriber", BOESchema);
+    const subscriber = new Subscriber({
+      First_Name: req.body.fname,
+      Last_Name: req.body.lname,
+      Email: req.body.email,
+      City: req.body.city,
+      Zipcode: req.body.zipcode,
+      SubscriberTradeOfInterest1: req.body.trade1,
+      SubscriberTradeOfInterest2: req.body.trade2,
+      SubscriberTradeOfInterest3: req.body.trade3,
+      Date: postedDate,
+    });
+
+    const err = subscriber.validateSync();
+    if (err) {
+      console.log(err.message);
+      req.flash("subError", err.message);
+      return res.redirect("/subscribe");
+    } else {
+      // validation passed
+      subscriber.save();
+      res.redirect("/success");
+    }
+  } catch {
+    res.redirect("/failure");
+  }
+});
+
+// -------------------------------- ROUTES ------------------------------------- //
 
 app.get("/", async (req, res) => {
   const schools = await School.find();
@@ -105,13 +252,17 @@ app.get("/", async (req, res) => {
 app.get("/subscribe", (req, res) => {
   const user = req.app.get("user");
   let name = ":name";
+  let email = "";
   if (user) {
     name = req.user.name;
+    email = req.user.email;
   }
   res.render("pages/subscribe", {
     name: name,
+    email: email,
     title: "Subscribe",
     path: "/subscribe",
+    subError: req.flash("subError"),
     isAuthenticated: req.isAuthenticated(),
   });
 });
@@ -271,7 +422,6 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
       month: "numeric",
       day: "numeric",
     });
-    const favSchools = [];
     const hashedPassword = await bcrypt.hash(
       req.body.password,
       bcrypt.genSaltSync(8)
@@ -282,7 +432,6 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
       date: postedDate,
-      favorites: favSchools,
     });
 
     const err = user.validateSync();
